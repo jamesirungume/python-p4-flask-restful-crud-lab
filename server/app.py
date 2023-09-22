bd8bac13529199eb2154f38b2d15b530b46dc71f
Your Name <jymohcooler@gmail.com>
2023-09-22 03:31:04 +0300

    Complete project

--- server/app.py
+++ server/app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -46,10 +47,37 @@ class PlantByID(Resource):
     def get(self, id):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
+    def patch(self, id):
+        
+        plant = Plant.query.filter_by(id=id).first() 
 
+        if not plant:
+            return make_response(jsonify({"error": "Plant not found"}), 404)
+
+        data = request.get_json()
+        if 'is_in_stock' in data:
+            plant.is_in_stock = data['is_in_stock']
+
+        db.session.commit()
+
+       
+        return make_response(jsonify(plant.to_dict()), 200)
+        
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        if not plant:
+            return make_response("", 204)
+
+        db.session.delete(plant)
+        db.session.commit()
+
+        
+        return make_response("", 204)
+        
+        
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
 
-if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+
